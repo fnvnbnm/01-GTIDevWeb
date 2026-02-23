40fee8ac1013ac95b25569c592877d7708f4207b
Transporte Uniaene <davianjos818@gmail.com>
2026-02-23 11:54:19 -0300

    feat: adiciona botão com alert

--- scripts.js
+++ scripts.js
@@ -1,2 +1,7 @@
+const botao = document.getElementById("btnAlerta");
+
+botao.addEventListener("click", function() {
+    alert("Parabéns! Você concluiu o desafio!");
+});
 // De propósito, este arquivo começa “quieto”.
 // O desafio pede uma alteração simples aqui.
